f7cc22e1162a285a358cab1211fc4753c0011ca7
David Oporto <opsa95@gmail.com>
2016-10-20 09:47:14 +0200

    index.js comented

--- js/control/index.js
+++ js/control/index.js
@@ -1,8 +1,9 @@
 /**
- *	jQuery program that generates an on screen keyboard, a clock
- *	and prints the date
+
  *	@author David Oporto i Sala
  *	@version 1.0
+ *	@description jQuery program that generates an on screen keyboard, a clock
+ *	and prints the date
  */
 $(document).ready(function() {
 	$("div").hide();
@@ -12,11 +13,15 @@ $(document).ready(function() {
 });
 
 /**
- *	This function creates a string with the day of the week, the number of the
+ *	@name generateDate
+ *	@date 14-09-16
+ *	@version 1.0
+ *	@author David Oporto i Sala
+ *	@description This function creates a string with the day of the week, the number of the
  *	day, the name of the month, and the year. Then it prints the string on screen.
  *	Also generates a digital clock which it's updated each second,
  *	showing the actual time of the client's computer.
- *	@author David Oporto i Sala
+ *	@params: none
  */
 function generateDate(){
 	$("#date").show();
@@ -38,10 +43,13 @@ function generateDate(){
 }
 
 /**
- *	This function generates the letters before the day depending on which day of
- *	the week i has been inputed.
- *	@param Day of the week that we get from the upper function
+ *	@name nth
+ *	@date 14-09-16
+ *	@version 1.0
  *	@author Rubén Arroyo
+ *	@description This function generates the letters before the day depending on which day of
+ *	the week i has been inputed.
+ *	@params: Day of the week that we get from the upper function
  */
 function nth (d){
   if(d>3 && d<21) return 'th';
@@ -54,13 +62,17 @@ function nth (d){
 }
 
 /**
- *	This function shows the keyboard div. Then creates an output textbox and
+ *	@name keyboardGenerator
+ *	@date 19-09-16
+ *	@version 1.0
+ *	@author David Oporto i Sala
+ *	@description This function shows the keyboard div. Then creates an output textbox and
  *	the keyboard (all the qwerty letters + CapsLock, Shift and Del). After
  *	creating the keybord it can check which kind of button you have pressed
  *	if it's a letter will print it onto the output textbox, if it's del will
  *	erase the last character of the output, the shift button makes the next letter
  *	inserted an upcase. The capsLock makes every letter in upcase until it's pressed again (disabled).
- *	@author David Oporto i Sala
+ *	@params: none
 */
 function keyboardGenerator(){
 	$("#keyboard").show();
