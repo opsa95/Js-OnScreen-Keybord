31e0c95f0c27489320f75bda778c05d34b253d24
David Oporto <opsa95@gmail.com>
2016-10-20 09:29:26 +0200

    index.js Modified

--- js/control/index.js
+++ js/control/index.js
@@ -1,88 +1,48 @@
 /**
-* TODO
-* @author David Oporto
-*/
-
-
-$(document).ready(function (){
-  $("div").hide();
-  startApp();
-
+ *	jQuery program that generates an on screen keyboard, a clock
+ *	and prints the date
+ *	@author David Oporto i Sala
+ *	@version 1.0
+ */
+$(document).ready(function() {
+	$("div").hide();
+	generateDate();
+	setInterval(function(){ generateDate(); }, 1000);
+	keyboardGenerator();
 });
 
-// Own Code
-
 /**
-* TODO
-*
-*/
-function startApp(){
-  $("#title").show();
-  $("#mainMenu").show();
-
-  $("#mainMenu [name=action]").click(function(){ // Inside the mainmenu div it searches all the tags with that name...
-    $("div").hide();
-    $("#title").show();
-    $("#mainMenu").show();
-
-    $("#"+$(this).val()).show();  // We concatenate to get the name of the id of the chosen radio button and show it
-
-    if($(this).val() == "calculator"){
-      generateCalculator();
-    }
-  });
-
-}
-
-/**
-* TODO
-*
-*/
-function generateCalculator(){
-
-  generateDay();
-
-  var calculatorButtons = [7,8,9,"/",4,5,6,"*",1,2,3,"-",0,".","=","+"];
-  var calculatorContent = "";
-//  var operationInput = "";
-
-  for (var i = 0; i < calculatorButtons.length; i++) {
-    calculatorContent += '<button type ="button" id="btn-success" class= "btn-success calculatorButton">'+calculatorButtons[i]+'</button>';
-    if (i==3 || i==7 || i==11 || i == 15)
-      calculatorContent += '</br>';
-  }
-  calculatorContent += '</br><input type ="text" id="output" readonly/>';
-  $("#calculator").append(calculatorContent);
-
-  $(".calculatorButton").click(function(){
-    if($(this).html()=="="){
-      $("#output").val(eval($("#output").val()));
-    } else{
-      $("#output").val($("#output").val() + $(this).html());
-    }
-
-  });
-
-}
-
-/**
-*
-*/
-function generateDay(){
+ *	This function creates a string with the day of the week, the number of the
+ *	day, the name of the month, and the year. Then it prints the string on screen.
+ *	Also generates a digital clock which it's updated each second,
+ *	showing the actual time of the client's computer.
+ *	@author David Oporto i Sala
+ */
+function generateDate(){
+	$("#date").show();
+	var dateContent = "";
   var date = new Date();
   var dateString = date.toString();
   var dayDate = date.getDate();
   var dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   var dayIndex = date.getDay();
-  var monthDate = ["January", "February", "March", "April", "May", "June", "July","Augusy","September","October","November","Desember"];
+  var monthDate = ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"];
   var yearDate = date.getFullYear();
 
-  $("#calculator").html('<div>'+'Today is: '+dayName[dayIndex]+' '+dayDate+nth(dayDate)
-  +' of '+ monthDate[date.getMonth()]+ ' '+yearDate+'</div>');
+  dateContent += '<div>'+'Today is: '+dayName[dayIndex]+' '+dayDate+nth(dayDate)
+  +' of '+ monthDate[date.getMonth()]+ ' '+yearDate;
+
+	dateContent += '</br>'+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+'</div>';
+	$("#date").html(dateContent);
 
 }
 
-// Add the end of the day add the corresponding letters
+/**
+ *	This function generates the letters before the day depending on which day of
+ *	the week i has been inputed.
+ *	@param Day of the week that we get from the upper function
+ *	@author Rubén Arroyo
+ */
 function nth (d){
   if(d>3 && d<21) return 'th';
   switch (d % 10){
@@ -90,7 +50,67 @@ function nth (d){
     case 2: return 'nd';
     case 3: return 'rd';
     default: 'th';
-
   }
+}
+
+/**
+ *	This function shows the keyboard div. Then creates an output textbox and
+ *	the keyboard (all the qwerty letters + CapsLock, Shift and Del). After
+ *	creating the keybord it can check which kind of button you have pressed
+ *	if it's a letter will print it onto the output textbox, if it's del will
+ *	erase the last character of the output, the shift button makes the next letter
+ *	inserted an upcase. The capsLock makes every letter in upcase until it's pressed again (disabled).
+ *	@author David Oporto i Sala
+*/
+function keyboardGenerator(){
+	$("#keyboard").show();
+	var keyboardContent="";
+	var keyboardButtons=["q","w","e","r","t","y","u","i","o","p","Del","CapsLock","a","s","d","f","g","h","j","k","l","ñ","Shift","z","x","c","v","b","n","m"];
+	var caps = false;
+	var capsLock = false;
+
+	// Creating the keyboard...
+	keyboardContent += '<input type ="text" id="output" readonly/><br/>';
+	for (var i=0;i<keyboardButtons.length;i++){
+		keyboardContent += '<button type ="button" id="btn-success" class="btn-success keyboardButton">'+keyboardButtons[i]+'</button>';
+		if (keyboardButtons[i]=="Del"||keyboardButtons[i]=="ñ"){
+			keyboardContent += '<br />';
+		}
+	}
+	$("#keyboard").append(keyboardContent);
+
+	// When you press a button...
+	$(".keyboardButton").click(function(){
+		var inputed = $(this).html();
+    if($(this).html()=="CapsLock"){	// Pressing the CapsLock button...
+				if (!capsLock){
+					caps = true;
+					capsLock = true;
+				} else if (capsLock){
+					capsLock = false;
+					caps = false;
+				}
+
+    } else if ($(this).html()=="Shift"){ // Pressing the Shift button...
+				if (!capsLock) caps = true;
+ 				else caps = false;
+
+		} else if ($(this).html()=="Del"){ // Pressing the Del button...
+			var getOutput = $("#output").val();
+			var newOutput = getOutput.substr(0,getOutput.length-1);
+			$("#output").val(newOutput);
+
+		} else{	// If it's a letter...
+			if (!caps){
+				$("#output").val($("#output").val() + inputed);
+				if(capsLock) caps=true;
+			} else {
+				$("#output").val($("#output").val() + inputed.toUpperCase());
+				if (!capsLock) caps=false;
+			}
+
+    }
+
+  });
 
 }
